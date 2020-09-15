7894d2259d825191c1060cd4b09b4b0af40e8933
Dariel <dariel901231@gmail.com>
2020-09-15 18:34:54 -0400

    Adding minor fixes, modifying adding a custom form hook for handle generic initial state of components and form inputs validations

--- frontend/src/places/pages/UpdatePlace.js
+++ frontend/src/places/pages/UpdatePlace.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Input from '../../shared/components/FormElements/Input';
 import Button from '../../shared/components/FormElements/Button';
-import { VALIDATOR_REQUIRE, VALIDATOR_MAXLENGTH } from '../../shared/util/validators';
-import { useForm } from '../../shared/hooks/form-hook'
+import Card from '../../shared/components/UIElements/Card';
+import {
+  VALIDATOR_REQUIRE,
+  VALIDATOR_MAXLENGTH,
+} from '../../shared/util/validators';
+import { useForm } from '../../shared/hooks/form-hook';
 
-import './PlaceForm.css'
+import './PlaceForm.css';
 
 const DUMMY_PLACES = [
   {
@@ -36,64 +40,99 @@ const DUMMY_PLACES = [
   },
 ];
 
-const UpdatePlace = () => { 
-
+const UpdatePlace = () => {
+  const [isLoading, setIsLoading] = useState(true);
   const placeId = useParams().placeId;
+
+  const [formState, inputHandler, setFormData] = useForm(
+    {
+      title: {
+        value: '',
+        isValid: false,
+      },
+      description: {
+        value: '',
+        isValid: false,
+      },
+      address: {
+        value: '',
+        isValid: false,
+      },
+    },
+    true
+  );
 
   const indentifiedPlace = DUMMY_PLACES.find((place) => place.id === placeId);
 
-  const [formState, inputHandler] = useForm({
-    title:{
-      value: indentifiedPlace.title,
-      isValid: true,
-    },
-    description:{
-      value: indentifiedPlace.description,
-      isValid: true,
-    },
-    address:{
-      value: indentifiedPlace.address,
-      isValid: true,
-    }
-  }, true);
+  useEffect(() => {
+      if(indentifiedPlace){
+        setFormData(
+            {
+              title: {
+                value: indentifiedPlace.title,
+                isValid: true,
+              },
+              description: {
+                value: indentifiedPlace.description,
+                isValid: true,
+              },
+            },
+            true
+          );
+      }
+    setIsLoading(false);
+  }, [setFormData, indentifiedPlace]);
 
-  const placeUpdateSubmitHandler = event => {
-      event.preventDefault();
-      console.log(formState.inputs)
-  }
+  const placeUpdateSubmitHandler = (event) => {
+    event.preventDefault();
+    console.log(formState.inputs);
+  };
 
   if (!indentifiedPlace) {
     return (
       <div className="center">
-        <h2>Could not find place!</h2>
+          <Card>
+            <h2>Could not find place!</h2>
+          </Card>
       </div>
     );
   }
+
+  if (isLoading) {
+    return (
+      <div className="center">
+        <h2>Loading...</h2>
+      </div>
+    );
+  }
+
   return (
-    <form className="place-form" onSubmit={placeUpdateSubmitHandler}>
-      <Input
-        id="title"
-        element="input"
-        type="text"
-        label="Title"
-        validators={[VALIDATOR_REQUIRE()]}
-        errorText="Please enter a valid title"
-        onInput={inputHandler}
-        initialValue={formState.inputs.title.value}
-        initialValid={formState.inputs.title.isValid}
-      />
-      <Input
-        id="description"
-        element="textarea"
-        label="Description"
-        validators={[VALIDATOR_MAXLENGTH(5)]}
-        errorText="Please enter a valid description (at least 5 characters)"
-        onInput={inputHandler}
-        initialValue={formState.inputs.description.value}
-        initialValid={formState.inputs.description.isValid}
-      />
-      <Button type="submit" disabled={!formState.isValid}>UPDATE PLACE</Button>
-    </form>
+      <form className="place-form" onSubmit={placeUpdateSubmitHandler}>
+        <Input
+          id="title"
+          element="input"
+          type="text"
+          label="Title"
+          validators={[VALIDATOR_REQUIRE()]}
+          errorText="Please enter a valid title"
+          onInput={inputHandler}
+          initialValue={formState.inputs.title.value}
+          initialValid={formState.inputs.title.isValid}
+        />
+        <Input
+          id="description"
+          element="textarea"
+          label="Description"
+          validators={[VALIDATOR_MAXLENGTH(5)]}
+          errorText="Please enter a valid description (at least 5 characters)"
+          onInput={inputHandler}
+          initialValue={formState.inputs.description.value}
+          initialValid={formState.inputs.description.isValid}
+        />
+        <Button type="submit" disabled={!formState.isValid}>
+          UPDATE PLACE
+        </Button>
+      </form>
   );
 };
 
